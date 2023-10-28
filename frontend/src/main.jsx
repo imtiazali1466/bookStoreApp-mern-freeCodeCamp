50e5f736619930dd6430ab6a10452b67e3b09b21
imtiazali1466 <imtiazali1466@gmail.com>
2023-10-29 00:21:15 +0500

    react routes implemented and checked

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// import CreateBooks from './pages/CreateBooks.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import CreateBooks from './pages/CreateBooks.jsx';
+import DeleteBook from './pages/DeleteBook.jsx';
+import EditBook from './pages/EditBook.jsx';
+import Home from './pages/Home.jsx';
+import ShowBook from './pages/ShowBook.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/books/create",
+    element: <CreateBooks/>
+  },
+  {
+    path: "/books/details/:id",
+    element: <ShowBook/>,
+  },
+  {
+    path: "/books/edit/:id",
+    element: <EditBook/>,
+  },
+  {
+    path: "/books/delete/:id",
+    element: <DeleteBook/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
